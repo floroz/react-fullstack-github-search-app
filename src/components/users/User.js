b2f46e54cb01be03e721ce7e4de1094e330c7d8a
floroz <tortora87@gmail.com>
2019-06-26 18:02:36 +0100

    switch laptop

--- src/components/users/User.js
+++ src/components/users/User.js
@@ -44,6 +44,18 @@ export default class User extends Component {
 				) : (
 					<i className="fas fa-times text-danger" />
 				)}
+				<div className="card grid-2">
+					<div className="all-center">
+						<img
+							src={avatar_url}
+							alt=""
+							className="round-img"
+							style={{ width: '150px' }}
+						/>
+						<h1>{name}</h1>
+						<p>{location}</p>
+					</div>
+				</div>
 			</>
 		);
 	}
